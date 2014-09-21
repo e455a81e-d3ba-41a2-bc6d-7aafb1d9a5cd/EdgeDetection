28d9f5a8069883db5561905bdd1081686143d486
Leonhardt Schwarz <if12b076@technikum-wien.at>
2014-09-21 22:31:55 +0200

    sobel implementiert

--- main.cpp
+++ main.cpp
@@ -3,37 +3,42 @@
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 
-const std::vector< std::vector<int>> sobel_h_kernel =            {{ 1,    0,    1}, { 2,    0,    2}, { 1,    0,    1}};
+cv::Mat ApplyEdgeDetection(cv::Mat& src, const std::vector<std::vector<int>>& hKernel, const std::vector<std::vector<int>>& vKernel);
+cv::Mat ImageConvolute(cv::Mat& src, const std::vector<std::vector<int>>& kernel, const double multiplier);
+
+const std::vector< std::vector<int>> sobel_h_kernel =            {{ 1,    0,   -1}, { 2,    0,   -2}, { 1,    0,   -1}};
 const std::vector< std::vector<int>> sobel_v_kernel =            {{ 1,    2,    1}, { 0,    0,    0}, {-1,   -2,   -1}};
 const std::vector< std::vector<int>> sharpen_kernel =            {{ 0,   -1,    0}, {-1,    5,   -1}, { 0,   -1,    0}};
 const std::vector< std::vector<int>> edge_kernel_weak =          {{ 1,    0,   -1}, { 0,    0,    0}, {-1,    0,    0}};
 const std::vector< std::vector<int>> edge_kernel_normal =        {{ 0,    1,    0}, { 1,   -4,    1}, { 0,    1,    0}};
 const std::vector< std::vector<int>> edge_kernel_strong =        {{-1,   -1,   -1}, {-1,    8,   -1}, {-1,   -1,   -1}};
+const std::vector< std::vector<int>> gaussian_blur =             {{ 1,    2,    1}, { 2,    4,    2}, { 1,    2,    1}};
 
-inline static uchar CheckBorderPixel(cv::Mat src, int x, int y, int ch) {
+
+inline static uchar CheckBorderGetPixel(cv::Mat src, int x, int y, int ch) {
         if ((x < 0) || (x >= src.cols) || (y < 0) || (y >= src.rows)) {
                 return 0;
         } else {
                 return src.at<cv::Vec3b>(y, x)[ch];
         }
 }
-                                 
-cv::Mat ImageConvolute(cv::Mat src, std::vector<std::vector<int>> kernel)
+
+cv::Mat ImageConvolute(cv::Mat& src, const std::vector<std::vector<int>>& kernel, const double multiplier = 1)
 {
         cv::Mat outImage(src.rows, src.cols, CV_8UC3);
-        
+        int size = kernel.size() / 2;
         
         for (int x = 0; x < src.cols ; x++)
         {
                 for (int y = 0; y < src.rows; y++) {
-                        int accumulator[3] = {0};
+                        double accumulator[3] = {0};
                         
-                        for (int kx = -1; kx < 2; kx++) {
-                                for (int ky = -1; ky < 2; ky++) {
+                        for (int kx = -size; kx <= size; kx++) {
+                                for (int ky = -size; ky <= size; ky++) {
                                         
-                                       for(int ch = 0; ch < 3; ch++) {
+                                       for (int ch = 0; ch < 3; ch++) {
                                                //auto tmp = src.at<cv::Vec3b>(y, x)[ch];
-                                               accumulator[ch] += CheckBorderPixel(src, x + kx, y + ky, ch) * kernel[ky + 1][kx + 1];
+                                               accumulator[ch] += multiplier * CheckBorderGetPixel(src, x + kx, y + ky, ch) * kernel[ky + size][kx + size];
                                        }
                                         
                                 }
@@ -45,7 +50,7 @@ cv::Mat ImageConvolute(cv::Mat src, std::vector<std::vector<int>> kernel)
                                        accumulator[ch] = 255;
                                else if (accumulator[ch] < 0)
                                        accumulator[ch] = 0;
-                              pixel[ch] = accumulator[ch]; 
+                              pixel[ch] = static_cast<uchar>(accumulator[ch]); 
                        }
                        outImage.at<cv::Vec3b>(y, x) = pixel;
                        
@@ -55,15 +60,58 @@ cv::Mat ImageConvolute(cv::Mat src, std::vector<std::vector<int>> kernel)
         return outImage;
 }
 
+cv::Mat ApplyEdgeDetection(cv::Mat& src, const std::vector<std::vector<int>>& hKernel, const std::vector<std::vector<int>>& vKernel) 
+{
+        
+        cv::Mat outImage(src.rows, src.cols, CV_8UC3);
+        int size = 1;
+        
+        for (int x = 0; x < src.cols ; x++)
+        {
+                for (int y = 0; y < src.rows; y++) {
+                        double hAccumulator[3] = {0};
+                        double vAccumulator[3] = {0};
+                        
+                        for (int kx = -size; kx <= size; kx++) {
+                                for (int ky = -size; ky <= size; ky++) {
+                                        
+                                       for (int ch = 0; ch < 3; ch++) {
+                                               //auto tmp = src.at<cv::Vec3b>(y, x)[ch];
+                                               hAccumulator[ch] += CheckBorderGetPixel(src, x + kx, y + ky, ch) * hKernel[ky + size][kx + size];
+                                               vAccumulator[ch] += CheckBorderGetPixel(src, x + kx, y + ky, ch) * vKernel[ky + size][kx + size];
+                                       }
+                                        
+                                }
+                                
+                        }
+                       cv::Vec3b pixel(0,0,0); 
+                       for(int ch = 0; ch < 3; ch++) {
+                               
+                               auto tmp = sqrt((hAccumulator[ch] * hAccumulator[ch]) + (vAccumulator[ch] * vAccumulator[ch]));
+                               
+                               if(tmp > 255)
+                                       tmp = 255;
+                               else if (tmp < 0)
+                                       tmp = 0;
+                               
+                              pixel[ch] = static_cast<uchar>(tmp); 
+                       }
+                       outImage.at<cv::Vec3b>(y, x) = pixel;
+                       
+                }
+                
+        }
+        return outImage;
+        
+}
+
 int main() {
 
         cv::namedWindow( "Test 1");
         auto src = cv::imread("TestData/lena.jpg");
-        auto dst = src.clone();
-        dst = ImageConvolute(src, sharpen_kernel);
+        auto dst = ImageConvolute(src, gaussian_blur, 0.0626);
+        dst = ApplyEdgeDetection(dst, sobel_h_kernel, sobel_v_kernel);
         cv::imshow("Test 1", dst);
-        std::cout << dst.cols << std::endl;
-        std::cout << dst.rows << std::endl;
         cv::waitKey(0);
         return 0;
 }
