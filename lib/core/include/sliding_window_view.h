ad303b7210997d69ef57cc20da142c81b1b963ac
leonhardt <lariscusObscurus@gmail.com>
2019-05-31 23:59:22 +0200

    Added sliding window iterator and tests

--- lib/core/include/sliding_window_view.h
+++ lib/core/include/sliding_window_view.h
@@ -1,18 +1,80 @@
 #pragma once
 #include<type_traits>
 #include<iterator>
+#include<functional>
 
 namespace lib
 {
 
-    template <std::size_t Height, std::size_t Width, std::size_t Stride, typename T>
+    template <std::size_t Height, std::size_t Width, typename T>
+    class sliding_window_view;
+
+    template <typename T, typename TContainer>
+    class sliding_window_iterator
+    {
+        using window = typename TContainer::window_type;
+
+        using difference_type   = std::size_t;
+        using reference         = window &;
+        using pointer           = window *;
+        using value_type        = window;
+        using iterator_category = std::bidirectional_iterator_tag;
+
+        difference_type idx_;
+        TContainer& view_;
+
+    public:
+        sliding_window_iterator(difference_type idx, TContainer& view) : idx_(idx), view_(view) { }
+
+        sliding_window_iterator(const sliding_window_iterator& other) : idx_(other.idx_), view_(other.view_) { }
+        sliding_window_iterator(sliding_window_iterator&& other) : idx_(std::move(other.idx_)), view_(std::move(other.view_)) { other.idx_ = 0; }
+
+
+        value_type operator*()
+        {
+            return view_[idx_];
+        }
+
+        sliding_window_iterator& operator++() noexcept
+        {
+            ++idx_;
+            return *this;
+        }
+
+        sliding_window_iterator operator++(int) noexcept
+        {
+            auto state = *this;
+            ++idx_;
+            return state;
+        }
+
+        sliding_window_iterator& operator--() noexcept
+        {
+            --idx_;
+            return *this;
+        }
+
+        sliding_window_iterator operator--(int) noexcept
+        {
+            auto state = *this;
+            --idx;
+            return state;
+        }
+
+        friend bool operator!=(sliding_window_iterator lhs, sliding_window_iterator rhs)
+        {
+            return (lhs.idx_ != rhs.idx_);
+        }
+    };
+
+    template <std::size_t Height, std::size_t Width, typename T>
     class sliding_window_view
     {
-        static_assert(Height > 0 && Width > 0, "Height and width must be above zero.");
-
+        static_assert(Height != 0 && Width != 0, "Height and width can not be zero.");
+    public:
         class window
         {
-
+            using value_type = typename T::value_type;
             class window_row
             {
                 typename T::iterator iterator_;
@@ -21,6 +83,11 @@ namespace lib
                 window_row(typename T::iterator&& iterator) : iterator_(std::move(iterator)) {}
 
                 typename T::value_type& operator[](std::size_t col)
+                {
+                    return *(iterator_ + col);
+                }
+
+                const typename T::value_type& operator[](std::size_t col) const
                 {
                     return *(iterator_ + col);
                 }
@@ -37,17 +104,22 @@ namespace lib
             window_row operator[](std::size_t row)
             {
                 auto iterator = container_.begin();
-                iterator += idx_ + row * container_.rows();
+                iterator += idx_ + row * container_.cols();
                 return window_row(std::move(iterator));
             }
 
             const window_row operator[](std::size_t row) const
             {
                 auto iterator = container_.begin();
-                iterator += idx_ + row * container_.rows();
+                iterator += idx_ + row * container_.cols();
                 return window_row(std::move(iterator));
             }
         };
+
+        using iterator = sliding_window_iterator<T, sliding_window_view>;
+        using const_iterator = sliding_window_iterator<T, sliding_window_view>;
+        using value_type = T;
+        using window_type = window;
 
     private:
         T& container_;
@@ -70,5 +142,42 @@ namespace lib
         {
             return max_row_ * max_col_;
         }
+
+        iterator begin()
+        {
+            return iterator(0, *this);
+        }
+
+        iterator end()
+        {
+            return iterator(size(), *this);
+        }
+
+        const_iterator cbegin() const
+        {
+            return iterator(0, *this);
+        }
+
+        const_iterator cend() const
+        {
+            return iterator(size(), *this);
+        }
+
+        const_iterator begin() const
+        {
+            return cbegin();
+        }
+
+        const_iterator end() const
+        {
+            return cend();
+        }
+
     };
+
+    template<std::size_t Height, std::size_t Width, typename T>
+    sliding_window_view<Height, Width, T> make_sliding_window_view(T& container) 
+    {
+        return sliding_window_view<Height, Width, T>(container);
+    }
 }
--- test/core/sliding_window_view_test.cpp
+++ test/core/sliding_window_view_test.cpp
@@ -1,25 +1,132 @@
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
 #include <memory>
 #include <core/core.h>
 #include <algorithm>
 #include <iostream>
+#include <vector>
 
-TEST(sliding_window_iterator, sliding_window_iterator_slides_the_window)
+
+namespace test_helper
 {
-    auto data = std::make_unique<float[]>(64);
 
-    lib::array2d<float> array(8, 8, std::move(data));
+    template<std::size_t Height, std::size_t Width, typename T>
+    using window = typename lib::sliding_window_view<Height, Width, T>::window;
 
-    float x = 0;
+    template<std::size_t Height, std::size_t Width, typename T>
+    bool window_matches(const std::vector<typename T::value_type>& expected, const window<Height, Width, T>& actual)
+    {
+        for (int j = 0; j < Height; j++)
+            for (int i = 0; i < Width; i++)
+                if (actual[j][i] != expected[j * Width + i])
+                {
+                    std::cout << "Expected: '" << expected[j * Width + i] << "' but was '" << actual[j][i] << "'." << std::endl;
+                    return false;
+                }
+        return true;
+    }
+}
+
+TEST(sliding_window_view, returns_correct_window)
+{
+    auto data = std::make_unique<int[]>(64);
+
+    lib::array2d<int> array(8, 8, std::move(data));
+
+    int x = 0;
     for (auto& it : array) {
         it = x; 
         x++;
     }
 
-    lib::sliding_window_view<3,3, 1, lib::array2d<float>> view{array};
-    
-    for (int i = 0; i < view.size(); i++) {
-        std::cout << view[i][2][2] << " ";
+    auto view = lib::make_sliding_window_view<3, 3>(array);
+
+    auto expect = std::vector<int>
+    {   
+        0, 1, 2,
+        8, 9, 10,
+        16, 17, 18 
+    };
+
+    ASSERT_TRUE((test_helper::window_matches <3, 3, lib::array2d<int>>(expect, view[0])));
+
+    expect = std::vector<int>
+    {   
+        45, 46, 47,
+        53, 54, 55,
+        61, 62, 63
+    }; 
+
+    ASSERT_TRUE((test_helper::window_matches <3, 3, lib::array2d<int>>(expect, view[35])));
+
+    expect = std::vector<int>
+    {   
+        18, 19, 20,
+        26, 27, 28,
+        34, 35, 36
+    }; 
+
+    ASSERT_TRUE((test_helper::window_matches <3, 3, lib::array2d<int>>(expect, view[14])));
+}
+
+TEST(sliding_window_view, returns_correct_asymmetrical_window)
+{
+    auto data = std::make_unique<int[]>(48);
+
+    lib::array2d<int> array(4, 12, std::move(data));
+
+    int x = 0;
+    for (auto& it : array) {
+        it = x; 
+        x++;
     }
-    std::cout << std::endl;
+
+    auto view = lib::make_sliding_window_view<2, 8>(array);
+
+    auto expect = std::vector<int>
+    {   
+        0, 1, 2, 3, 4, 5, 6, 7,
+        12, 13, 14, 15, 16, 17, 18, 19
+    };
+
+    ASSERT_TRUE((test_helper::window_matches <2, 8, lib::array2d<int>>(expect, view[0])));
+
+    expect = std::vector<int>
+    {   
+        28, 29, 30, 31, 32, 33, 34, 35,
+        40, 41, 42, 43, 44, 45, 46, 47
+    };
+
+    ASSERT_TRUE((test_helper::window_matches <2, 8, lib::array2d<int>>(expect, view[14])));
+}
+
+TEST(sliding_window_view, supports_range_based_for)
+{
+    auto data = std::make_unique<int[]>(64);
+
+    lib::array2d<int> array(8, 8, std::move(data));
+
+    int x = 0;
+    for (auto& it : array) {
+        it = x; 
+        x++;
+    }
+
+    lib::sliding_window_view<3,3,lib::array2d<int>> view{array};
+
+    auto expect = std::vector<int>
+    {   
+        0, 1, 2, 3, 4, 5,
+        8, 9, 10, 11, 12, 13,
+        16, 17, 18, 19, 20, 21,
+        24, 25, 26, 27, 28, 29,
+        32, 33, 34, 35, 36, 37,
+        40, 41, 42, 43, 44, 45
+    };
+    
+    auto expected_value_at_position = expect.begin();
+    for (auto& it: view) {
+        EXPECT_EQ(*expected_value_at_position, it[0][0]);
+        expected_value_at_position++;
+    }
 }
