b6b83bcfacdcadd5a3070e6cec53778a429be431
leonhardt <lariscusObscurus@gmail.com>
2019-06-01 19:17:54 +0200

    Fixed incorrect float conversion.

--- lib/image/include/image_converter.h
+++ lib/image/include/image_converter.h
@@ -13,52 +13,111 @@ namespace lib {
     };
 
 
-    namespace internal {
+    namespace internal 
+    {
+        template<typename TTo,typename TFrom>
+        TTo inverse_map_floating_point(TFrom from) 
+        {
+            static_assert(std::is_floating_point_v<TFrom>);
+            static_assert(std::is_integral_v<TTo>);
+            return static_cast<TTo>(from * std::numeric_limits<TTo>::max());
+        }
 
-        template<typename TTo, grayscale_mode mode, typename TFrom>
-        TTo convert(const TFrom& from)
+        template<typename TTo,typename TFrom>
+        TTo map_floating_point(TFrom from) 
+        {
+            static_assert(std::is_integral_v<TFrom>);
+            static_assert(std::is_floating_point_v<TTo>);
+            // linear mapping from [0 numeric_limits<TFrom>::max] to [0.0 1.0]
+            return static_cast<TTo>(from) / static_cast<TTo>(std::numeric_limits<TFrom>::max());
+        }
+
+        template<typename TTo, grayscale_mode Mode, typename TFrom>
+        TTo convert_to_grayscale(TFrom from)
+        {
+            static_assert(Mode != grayscale_mode::None, "No grayscale mode chosen.");
+
+            if constexpr (Mode == grayscale_mode::Average)
+            {
+                return static_cast<TTo>((from.r + from.g + from.b) / static_cast<TTo>(3));
+            }
+            if constexpr (Mode == grayscale_mode::Lightness)
+            {
+                return static_cast<TTo>((std::max({ from.r, from.g, from.b }) + std::min({ from.r, from.g, from.b })) / static_cast<TTo>(2));
+            }
+            if constexpr (Mode == grayscale_mode::Luminosity)
+            {
+                return static_cast<TTo>(0.21 * from.r + 0.72 * from.g + 0.07 * from.b);
+            }
+        }
+
+
+        template<typename TTo, grayscale_mode Mode, typename TFrom>
+        inline TTo convert(const TFrom& from)
         {
             if constexpr (pixel_traits<TFrom>::is_rgb())
             {
                 if constexpr (pixel_traits<TFrom>::channels() == 3 && pixel_traits<TTo>::channels() == 3)
                 {
                     auto result = TTo{};
-                    result.r = static_cast<typename pixel_traits<TTo>::channel_type>(from.r);
-                    result.g = static_cast<typename pixel_traits<TTo>::channel_type>(from.g);
-                    result.b = static_cast<typename pixel_traits<TTo>::channel_type>(from.b);
+
+                    if constexpr (std::is_integral_v<typename pixel_traits<TFrom>::channel_type> && std::is_floating_point_v<typename pixel_traits<TTo>::channel_type>) 
+                    {
+                        result.r = map_floating_point<typename pixel_traits<TTo>::channel_type>(from.r);
+                        result.g = map_floating_point<typename pixel_traits<TTo>::channel_type>(from.g);
+                        result.b = map_floating_point<typename pixel_traits<TTo>::channel_type>(from.b);
+
+                    }
+                    else if constexpr (std::is_floating_point_v<typename pixel_traits<TFrom>::channel_type> && std::is_integral_v<typename pixel_traits<TTo>::channel_type>)
+                    {
+                        result.r = inverse_map_floating_point<typename pixel_traits<TTo>::channel_type>(from.r);
+                        result.g = inverse_map_floating_point<typename pixel_traits<TTo>::channel_type>(from.g);
+                        result.b = inverse_map_floating_point<typename pixel_traits<TTo>::channel_type>(from.b);
+                    }
+                    else 
+                    {
+                        result.r = static_cast<typename pixel_traits<TTo>::channel_type>(from.r);
+                        result.g = static_cast<typename pixel_traits<TTo>::channel_type>(from.g);
+                        result.b = static_cast<typename pixel_traits<TTo>::channel_type>(from.b);
+                    }
+
                     return result;
                 }
 
                 if constexpr (pixel_traits<TFrom>::channels() == 3 && pixel_traits<TTo>::channels() == 1)
                 {
-
-                    static_assert(mode != grayscale_mode::None, "No grayscale mode chosen.");
-
-                    if constexpr (mode == grayscale_mode::Average)
+                    if constexpr (std::is_integral_v<typename pixel_traits<TFrom>::channel_type> && std::is_floating_point_v<TTo>) 
                     {
-                        return static_cast<TTo>((from.r + from.g + from.b) / static_cast<TTo>(3));
+                        return map_floating_point<TTo>(convert_to_grayscale<typename pixel_traits<TFrom>::channel_type, Mode>(from));
                     }
-                    if constexpr (mode == grayscale_mode::Lightness)
+                    else 
                     {
-                        return static_cast<TTo>((std::max({ from.r, from.g, from.b }) + std::min({ from.r, from.g, from.b })) / static_cast<TTo>(2));
-                    }
-                    if constexpr (mode == grayscale_mode::Luminosity)
-                    {
-                        return static_cast<TTo>(0.21 * from.r + 0.72 * from.g + 0.07 * from.b);
+                        return convert_to_grayscale<TTo, Mode>(from);
                     }
                 }
             }
 
             if constexpr (pixel_traits<TFrom>::channels::value == 1 && pixel_traits<TTo>::channels::value == 1)
             {
-                return static_cast<TTo>(from);
+                if constexpr ( std::is_integral_v<TFrom> && std::is_floating_point_v<TTo>) 
+                {
+                    return map_floating_point<TTo>(from);
+                } 
+                else if constexpr (std::is_floating_point_v<TFrom> && std::is_integral_v<TTo>)
+                {
+                    return inverse_map_floating_point<TTo>(from);
+                }
+                else
+                {
+                    return static_cast<TTo>(from);
+                }
             }
         }
     }
 
 
-    template<typename TTo, grayscale_mode mode = grayscale_mode::None, typename TFrom>
-    array2d<TTo> convert(const array2d<TFrom>& input)
+    template<typename TTo, grayscale_mode mode = grayscale_mode::None, typename TFrom, typename Deleter>
+    array2d<TTo> convert(const array2d<TFrom, Deleter>& input)
     {
         auto output = array2d<TTo>(input.rows(), input.cols());
 
